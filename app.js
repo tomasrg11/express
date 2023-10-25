3eaa7a60550f20eff3833399d845eb615c82914e
tomasrg11 <tomramirezg11@gmail.com>
2023-10-25 15:03:36 -0500

    Se creo index.html

--- app.js
+++ app.js
@@ -5,3 +5,34 @@ let app = express();
 app.listen('3000',function(){
     console.log('Servidor ok');
 })
+
+app.get('/', function(req,res){
+    res.send('Ruta INICIO');
+})
+
+//crear 
+//objeto de la conexion--  se establecen los parámetros
+let conexion = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'articulosdb'
+});
+// Probar la conexión
+conexion.connect(function(error){
+   if(error){
+       throw error;
+   } else{
+       console.log('Conexión exitosa');
+   }
+});
+
+app.get('/api/articulos', (req,res)=>{
+    conexion.query('SELECT * FROM articulos', (error,filas)=>{
+        if(error){
+            throw error;
+        }else{
+            res.send(filas);
+        }
+    });
+});
